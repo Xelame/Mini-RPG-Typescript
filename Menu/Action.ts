69bcbfe4376a37580e4d0331c365945f6eda9a32
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-05-13 09:55:14 +0200

    réordonner le fight

---
+++ Menu/Action.ts
@@ -0,0 +1,54 @@
+import { Character } from "../Characters/Character.ts";
+import { Menu } from "./Menu.ts"
+import { CibleMenu } from "./Cible.ts"
+
+
+export class ActionMenu extends Menu {
+
+    character: Character;
+
+    enemies: Character[];
+
+    allies: Character[];
+
+    constructor(
+        character: Character,
+        enemies: Character[],
+        allies: Character[],
+    ) {
+        super("Choissisez une action", [
+            "Basic attack",
+            "Special attack",
+            "Use item",])
+        this.character = character;
+        this.enemies = enemies;
+        this.allies = allies
+        super.asking();
+    }
+
+    resolve(choice: string | null): void {
+        switch (choice) {
+            case "1":
+                this.character.attack(this.enemies[new CibleMenu(this.enemies).cible]);
+                break;
+            case "2":
+                if (!this.character.specialAttackOnAll(this.enemies)) {
+                    if (!this.character.specialAttackOnNothing()) {
+                        if (this.character.specialAttackOnEnnemy()) {
+                            this.character.specialAttackOnEnnemy(this.enemies[new CibleMenu(this.enemies).cible]);
+                        }
+                        if (this.character.specialAttackOnAlly()) {
+                            this.character.specialAttackOnAlly(this.allies[new CibleMenu(this.allies).cible]);
+                        } else {
+                            console.log("Nothing append");
+                            super.asking();
+                        }
+                    }
+                }
+                break;
+            case "3":
+
+                break;
+        }
+    }
+}
