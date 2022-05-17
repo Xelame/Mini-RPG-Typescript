d4627214ad4158dddac5c26bd659031f65c1ca58
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-05-17 16:34:38 +0200

    Repaired Paladin'special attack

--- Characters/Paladin.ts
+++ Characters/Paladin.ts
@@ -16,7 +16,8 @@ export class Paladin extends Character {
      */
     public specialAttackOnAll(targets: Character[]): boolean {
         for (let i = 0; i < targets.length; i++) {
-            this.currentHealth = Math.max((this.physicalAttack - targets[i].physicalArmor) * 0.4, 0)
+            let damage = Math.max((this.physicalAttack - targets[i].physicalArmor) * 0.4, 10)
+            targets[i].currentHealth = Math.max(targets[i].currentHealth - damage, 0)
             Math.round(this.currentHealth)
         }
         return true
